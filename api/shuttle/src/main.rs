db72ee4c11f1a11d7069ba45409b8e440036f182
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-07-17 18:03:58 +0900

    add instrumentasion

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -11,6 +11,8 @@ async fn hello_world() -> &'static str {
 
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+
+    tracing::info!("Getting version");
     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
         .fetch_one(db.get_ref())
         .await;
